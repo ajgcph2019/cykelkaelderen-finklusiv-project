02dfc1526be89ccaf20006827bacf0d9cb52c273
ajgcph2019 <annajohngeorge@gmail.com>
2021-01-22 16:10:30 +0530

    added files

--- src/components/Services.js
+++ src/components/Services.js
@@ -2,7 +2,7 @@ import React from "react";
 import product1 from "../assets/images/product1.jpg";
 import product6 from "../assets/images/product6.jpeg";
 import product3 from "../assets/images/product3.png";
-import product4 from "../assets/images/product4.jpeg";
+import pdt4 from "../assets/images/pdt4.jpeg";
 import product2 from "../assets/images/product2.png";
 import product5 from "../assets/images/product5.jpg";
 import arrow1 from "../assets/images/arrow1.png";
@@ -37,13 +37,13 @@ function Services() {
      
       <div className="images-content">
         <div className="img-item">
-          <img src={`${product4}`} alt="product4" />
+          <img src={`${product6}`} alt="product6" />
         </div>
         <div className="img-item">
           <img src={`${product5}`} alt="product5" />
         </div>
         <div className="img-item">
-          <img src={`${product6}`} alt="product6" />
+          <img src={`${pdt4}`} alt="product4" />
         </div>
       </div>
       
